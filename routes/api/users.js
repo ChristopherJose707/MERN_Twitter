b5a60e25a696b984c388a5809479a7396a62b970
chrisjose <christopherjose707@gmail.com>
2020-12-23 13:58:12 -0800

    Add tweet model

--- routes/api/users.js
+++ routes/api/users.js
@@ -65,46 +65,8 @@ router.post('/register', (req, res) => {
 })
 
 // LOGIN USER
-// router.post('/login', (req, res) => {
-//   const { errors, isValid } = validateLoginInput(req.body);
-//   if (!isValid) {
-//     return res.status(400).json(errors);
-//   }
-
-//   const email = req.body.email;
-//   const password = req.body.password;
-
-//   User.findOne({email})
-//     .then(user => {
-//       if (!user) {
-//         errors.handle = "This user does not exist"
-//         return res.status(404).json(errors);
-//       }
-
-//       bcrypt.compare(password, user.password)
-//         .then(isMatch => {
-//           if (isMatch) {
-//             const payload = {id: user.id, handle: user.handle};
-
-//             jwt.sign(payload, keys.secretOrKey, {expiresIn: 3600}, 
-//               (err, token) => {
-//                 res.json({
-//                   success: true,
-//                   token: 'Bearer ' + token
-//                 });
-//               }
-//             )
-//           } else {
-//             errors.password = "Incorrect password"
-//             return res.status(400).json(errors);
-//           }
-//         })
-//     })
-// })
-
-router.post("/login", (req, res) => {
+router.post('/login', (req, res) => {
   const { errors, isValid } = validateLoginInput(req.body);
-
   if (!isValid) {
     return res.status(400).json(errors);
   }
@@ -112,24 +74,33 @@ router.post("/login", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  User.findOne({ email }).then((user) => {
-    if (!user) {
-      // Use the validations to send the error
-      errors.email = "User not found";
-      return res.status(404).json(errors);
-    }
-
-    bcrypt.compare(password, user.password).then((isMatch) => {
-      if (isMatch) {
-        res.json({ msg: "Success" });
-      } else {
-        // And here:
-        errors.password = "Incorrect password";
-        return res.status(400).json(errors);
+  User.findOne({email})
+    .then(user => {
+      if (!user) {
+        errors.handle = "This user does not exist"
+        return res.status(404).json(errors);
       }
-    });
-  });
-});
+
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            const payload = {id: user.id, handle: user.handle};
+
+            jwt.sign(payload, keys.secretOrKey, {expiresIn: 3600}, 
+              (err, token) => {
+                res.json({
+                  success: true,
+                  token: 'Bearer ' + token
+                });
+              }
+            )
+          } else {
+            errors.password = "Incorrect password"
+            return res.status(400).json(errors);
+          }
+        })
+    })
+})
 
 
 router.get("/test", (req, res) => res.json({msg: "This is the users route"}));
